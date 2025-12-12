941c5f88b7499f5c9d7c0a385836fb493e9759ed
Suzanne Pons <suzanne.pons68@gmail.com>
2025-12-12 17:01:59 +0100

    Ajout d'une base header, footer et home(=>hero)

---
+++ migrations/Version20251212153239.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251212153239 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, comment_title VARCHAR(80) NOT NULL, publication_date DATETIME NOT NULL, message LONGTEXT NOT NULL, realted_post_id INT DEFAULT NULL, comment_user_id INT DEFAULT NULL, INDEX IDX_9474526CAD13A819 (realted_post_id), INDEX IDX_9474526C541DB185 (comment_user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE network (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, link VARCHAR(255) NOT NULL, network_user_id INT DEFAULT NULL, INDEX IDX_608487BC3DD04FF (network_user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, post_title VARCHAR(150) NOT NULL, publication_date DATETIME NOT NULL, textuel_content LONGTEXT NOT NULL, visuel_content VARCHAR(255) DEFAULT NULL, user_of_post_id INT DEFAULT NULL, INDEX IDX_5A8A6C8D119B3471 (user_of_post_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE post_tag (post_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_5ACE3AF04B89032C (post_id), INDEX IDX_5ACE3AF0BAD26311 (tag_id), PRIMARY KEY (post_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE sub_comment (id INT AUTO_INCREMENT NOT NULL, publication_date DATETIME NOT NULL, sub_comment_title VARCHAR(80) DEFAULT NULL, message LONGTEXT NOT NULL, comment_id INT DEFAULT NULL, sub_comment_user_id INT DEFAULT NULL, INDEX IDX_EBAC0E98F8697D13 (comment_id), INDEX IDX_EBAC0E984EA6C1AA (sub_comment_user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, tag_name VARCHAR(80) NOT NULL, creation_date DATETIME NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(80) NOT NULL, avatar VARCHAR(255) DEFAULT NULL, inscription_date DATETIME NOT NULL, `describe` LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE user_tag (user_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_E89FD608A76ED395 (user_id), INDEX IDX_E89FD608BAD26311 (tag_id), PRIMARY KEY (user_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CAD13A819 FOREIGN KEY (realted_post_id) REFERENCES post (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C541DB185 FOREIGN KEY (comment_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE network ADD CONSTRAINT FK_608487BC3DD04FF FOREIGN KEY (network_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D119B3471 FOREIGN KEY (user_of_post_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE post_tag ADD CONSTRAINT FK_5ACE3AF04B89032C FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE post_tag ADD CONSTRAINT FK_5ACE3AF0BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE sub_comment ADD CONSTRAINT FK_EBAC0E98F8697D13 FOREIGN KEY (comment_id) REFERENCES comment (id)');
+        $this->addSql('ALTER TABLE sub_comment ADD CONSTRAINT FK_EBAC0E984EA6C1AA FOREIGN KEY (sub_comment_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE user_tag ADD CONSTRAINT FK_E89FD608A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_tag ADD CONSTRAINT FK_E89FD608BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CAD13A819');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C541DB185');
+        $this->addSql('ALTER TABLE network DROP FOREIGN KEY FK_608487BC3DD04FF');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D119B3471');
+        $this->addSql('ALTER TABLE post_tag DROP FOREIGN KEY FK_5ACE3AF04B89032C');
+        $this->addSql('ALTER TABLE post_tag DROP FOREIGN KEY FK_5ACE3AF0BAD26311');
+        $this->addSql('ALTER TABLE sub_comment DROP FOREIGN KEY FK_EBAC0E98F8697D13');
+        $this->addSql('ALTER TABLE sub_comment DROP FOREIGN KEY FK_EBAC0E984EA6C1AA');
+        $this->addSql('ALTER TABLE user_tag DROP FOREIGN KEY FK_E89FD608A76ED395');
+        $this->addSql('ALTER TABLE user_tag DROP FOREIGN KEY FK_E89FD608BAD26311');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE network');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE post_tag');
+        $this->addSql('DROP TABLE sub_comment');
+        $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE user_tag');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
