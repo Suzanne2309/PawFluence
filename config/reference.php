8f3749dd28e9e539702eb0aa70e84844f0cf93b9
Suzanne Pons <suzanne.pons68@gmail.comgit config --global user.email suzanne.pons68@gmail.com>
2025-12-19 09:30:11 +0100

    installation de tailwind sur nouveau ordinateur

--- config/reference.php
+++ config/reference.php
@@ -1498,6 +1498,16 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  * @psalm-type SymfonycastsVerifyEmailConfig = array{
  *     lifetime?: int, // The length of time in seconds that a signed URI is valid for after it is created. // Default: 3600
  * }
+ * @psalm-type WebpackEncoreConfig = array{
+ *     output_path: scalar|null, // The path where Encore is building the assets - i.e. Encore.setOutputPath()
+ *     crossorigin?: false|"anonymous"|"use-credentials", // crossorigin value when Encore.enableIntegrityHashes() is used, can be false (default), anonymous or use-credentials // Default: false
+ *     preload?: bool, // preload all rendered script and link tags automatically via the http2 Link header. // Default: false
+ *     cache?: bool, // Enable caching of the entry point file(s) // Default: false
+ *     strict_mode?: bool, // Throw an exception if the entrypoints.json file is missing or an entry is missing from the data // Default: true
+ *     builds?: array<string, scalar|null>,
+ *     script_attributes?: array<string, scalar|null>,
+ *     link_attributes?: array<string, scalar|null>,
+ * }
  * @psalm-type StimulusConfig = array{
  *     controller_paths?: list<scalar|null>,
  *     controllers_json?: scalar|null, // Default: "%kernel.project_dir%/assets/controllers.json"
@@ -1512,16 +1522,6 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *     },
  *     default_transport?: scalar|null, // Default: "default"
  * }
- * @psalm-type WebpackEncoreConfig = array{
- *     output_path: scalar|null, // The path where Encore is building the assets - i.e. Encore.setOutputPath()
- *     crossorigin?: false|"anonymous"|"use-credentials", // crossorigin value when Encore.enableIntegrityHashes() is used, can be false (default), anonymous or use-credentials // Default: false
- *     preload?: bool, // preload all rendered script and link tags automatically via the http2 Link header. // Default: false
- *     cache?: bool, // Enable caching of the entry point file(s) // Default: false
- *     strict_mode?: bool, // Throw an exception if the entrypoints.json file is missing or an entry is missing from the data // Default: true
- *     builds?: array<string, scalar|null>,
- *     script_attributes?: array<string, scalar|null>,
- *     link_attributes?: array<string, scalar|null>,
- * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1534,9 +1534,9 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *     security?: SecurityConfig,
  *     monolog?: MonologConfig,
  *     symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
+ *     webpack_encore?: WebpackEncoreConfig,
  *     stimulus?: StimulusConfig,
  *     turbo?: TurboConfig,
- *     webpack_encore?: WebpackEncoreConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1552,9 +1552,9 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         monolog?: MonologConfig,
  *         maker?: MakerConfig,
  *         symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
+ *         webpack_encore?: WebpackEncoreConfig,
  *         stimulus?: StimulusConfig,
  *         turbo?: TurboConfig,
- *         webpack_encore?: WebpackEncoreConfig,
  *     },
  *     "when@prod"?: array{
  *         imports?: ImportsConfig,
@@ -1568,9 +1568,9 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         security?: SecurityConfig,
  *         monolog?: MonologConfig,
  *         symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
+ *         webpack_encore?: WebpackEncoreConfig,
  *         stimulus?: StimulusConfig,
  *         turbo?: TurboConfig,
- *         webpack_encore?: WebpackEncoreConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1585,9 +1585,9 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         security?: SecurityConfig,
  *         monolog?: MonologConfig,
  *         symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
+ *         webpack_encore?: WebpackEncoreConfig,
  *         stimulus?: StimulusConfig,
  *         turbo?: TurboConfig,
- *         webpack_encore?: WebpackEncoreConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
